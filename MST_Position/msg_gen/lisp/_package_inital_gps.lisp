3db317040838f7ae7f496e63b340c49edbddedb5
Robo Joe <missouristrobotics@yahoo.com>
2011-09-06 16:12:44 -0500

    removed build files - James Anderson

--- MST_Position/msg_gen/lisp/_package_inital_gps.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package MST_Position-msg)
-(cl:export '(TARGET_HEADING-VAL
-          TARGET_HEADING
-          DISTANCE-VAL
-          DISTANCE
-          WAYPOINT-VAL
-          WAYPOINT
-))
--- mst_common/msg_gen/lisp/_package_Velocity.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package mst_common-msg)
-(cl:export '(LINEAR-VAL
-          LINEAR
-          ANGULAR-VAL
-          ANGULAR
-))
