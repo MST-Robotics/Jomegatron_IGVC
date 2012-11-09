88e480ead32d42dc103db91ef7ea577b769ca78a
Robot <arxk5@mst.edu>
2012-11-08 18:47:12 -0600

    jpeg: Finished basic structure of A* grid mapping and made it actually build, still need actual mapping code

---
+++ mst_astar/src/constants.h
@@ -0,0 +1,19 @@
+/*******************************************************************************
+* @file constants.h
+* @author Jason Gassel <jmgkn6@mst.edu>
+* @version 1.0
+* @date 11/8/12
+* @brief Constants related to A* navigation and mapping.
+******************************************************************************/
+
+const float MAP_RESOLUTION = 1.0;
+const uint32_t MAP_WIDTH = 10;
+const uint32_t MAP_HEIGHT = 10;
+const double MAP_ORIGIN_POSITION_X = 0.0;
+const double MAP_ORIGIN_POSITION_Y = 0.0;
+const double MAP_ORIGIN_POSITION_Z = 0.0;
+const double MAP_ORIGIN_ORIENTATION_X = 0.0;
+const double MAP_ORIGIN_ORIENTATION_Y = 0.0;
+const double MAP_ORIGIN_ORIENTATION_Z = 0.0;
+const double MAP_ORIGIN_ORIENTATION_W = 0.0;
+
--- mst_astar/src/gridMapping.cpp
+++ mst_astar/src/gridMapping.cpp
@@ -3,16 +3,22 @@
 * @author Jason Gassel <jmgkn6@mst.edu>
 * @version 1.0
 * @date 10/25/12
-* @brief Generates a 2D map from laser scan and odom messages.
+* @brief Generates a 2D grid map from laser scan and odom messages.
 ******************************************************************************/
 #include "ros/ros.h"
 
+#include "nav_msgs/Odometry.h"
 #include "sensor_msgs/LaserScan.h"
 #include "nav_msgs/OccupancyGrid.h"
 
+#include "constants.h"
+
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
-void positionCallback(const nav_msgs::Odometry::ConstPtr& msg);
 void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+
+nav_msgs::Odometry odom;
+sensor_msgs::LaserScan laserScan;
+nav_msgs::OccupancyGrid grid;
 
 int main(int argc, char **argv)
 {
@@ -22,18 +28,51 @@ int main(int argc, char **argv)
     ros::Rate loop_rate(2);   
     
     //Subscribe to the messages we need
-    ROS::Subscriber s_odom = n.subscribe("/odom", 1, &odomCallback, this);
-    ROS::Subscriber s_position = n.subscribe("/position", 1, &positionCallback, this);
-    ROS::Subscriber s_laserScan = n.subscribe("/laser_scan", 1, &laserScanCallback, this);
+    ros::Subscriber s_odom = n.subscribe("robot_pose_ekf/odom_combined", 1, &odomCallback);
+    ros::Subscriber s_laserScan = n.subscribe("/laser_scan", 1, &laserScanCallback);
 
     //Advertise our message
-    ROS::Publisher p_map = n.advertise<nav_msgs::OccupancyGrid>("/map", 5) 
+    ros::Publisher p_map = n.advertise<nav_msgs::OccupancyGrid>("/map", 5); 
  
+    //Initialize our occupancy grid message
+    grid.info.resolution = MAP_RESOLUTION;
+    grid.info.width = MAP_WIDTH;
+    grid.info.height = MAP_HEIGHT;
+    grid.info.origin.position.x = MAP_ORIGIN_POSITION_X;
+    grid.info.origin.position.y = MAP_ORIGIN_POSITION_Y;
+    grid.info.origin.position.z = MAP_ORIGIN_POSITION_Z;
+    grid.info.origin.orientation.x = MAP_ORIGIN_ORIENTATION_X;
+    grid.info.origin.orientation.y = MAP_ORIGIN_ORIENTATION_Y;
+    grid.info.origin.orientation.z = MAP_ORIGIN_ORIENTATION_Z;
+    grid.info.origin.orientation.w = MAP_ORIGIN_ORIENTATION_W;
+    for(unsigned int i=0; i<MAP_WIDTH*MAP_HEIGHT; i++)
+        grid.data.push_back(1);
+
     while(ros::ok())
     {
+        //TODO Update map
+        
+        //Publish map
+        p_map.publish(grid);
+        
         ros::spinOnce();
         loop_rate.sleep();
     }
+    
+    s_odom.shutdown();
+    s_laserScan.shutdown();
+    p_map.shutdown();
+    
+    return 0;
 }
 
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+    odom = *msg;
+}
+
+void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+    laserScan = *msg;
+}
 
